3cc3f0000ff76c38a2193858af76809d64c56144
madongdong <madongdong2015@163.com>
2018-01-18 23:14:20 +0800

    ribbon实现客户端负载均衡(区别于服务端负载均衡)

--- java-eureka-client/src/main/java/com/dongchen/java/eurekaclient/EurekaClientApplication.java
+++ java-eureka-client/src/main/java/com/dongchen/java/eurekaclient/EurekaClientApplication.java
@@ -3,6 +3,9 @@ package com.dongchen.java.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author: mdd
@@ -15,5 +18,11 @@ public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
 
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
--- java-eureka-client/src/main/java/com/dongchen/java/eurekaclient/controller/TestController.java
+++ java-eureka-client/src/main/java/com/dongchen/java/eurekaclient/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.dongchen.java.eurekaclient.controller;
 
 import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.converters.Auto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
@@ -17,9 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/hello")
     public String index(){
         return "eureka client";
+    }
+
+    @GetMapping("/ribbon-consumer")
+    public String helloConsumer(){
+        return restTemplate.getForEntity("http://HELLO-SERVICE/client-hello",String.class).getBody();
+//        return "eureka client";
     }
 }
