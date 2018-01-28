f0f5623fc607318010298263e434d67c9f8130ee
madongdong <madongdong2015@163.com>
2018-01-28 17:02:15 +0800

    增加一个helloworld模块便于测试

---
+++ hello-service/src/main/java/com/dongchen/java/eurekaclientTest/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.dongchen.java.eurekaclientTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: mdd
+ * @date:2017/12/7
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+
+    }
+}
---
+++ hello-service/src/main/java/com/dongchen/java/eurekaclientTest/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.dongchen.java.eurekaclientTest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * @author: mdd
+ * @date:2018/1/17
+ * @描述:
+ */
+@RestController
+public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @GetMapping("/hello")
+    public String index(){
+
+        return "eureka client";
+    }
+
+}
